73384408dae2c688d8ae6b8e10592114d5545f42
Patrick Arminio <patrick.arminio@gmail.com>
2022-12-14 19:17:08 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::net::Ipv4Addr;
+
+use axum::Server;
+use tracing::info;
+
+use demo::app;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let app = app();
+    let port = std::env::var("PORT")
+        .unwrap_or_else(|_| "4001".to_string())
+        .parse::<u16>()
+        .unwrap();
+    info!(
+        "Run `rover dev --url http://localhost:{port} --name {crate_name}`",
+        crate_name = env!("CARGO_PKG_NAME")
+    );
+
+    Server::bind(&(Ipv4Addr::new(0, 0, 0, 0), port).into())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
